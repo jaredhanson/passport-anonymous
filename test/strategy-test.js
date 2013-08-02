1c929fe56e6d73902e51338e607f1f6a08b4a7d4
Jared Hanson <jaredhanson@gmail.com>
2013-08-01 23:03:09 -0700

    Fix tests.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -1,7 +1,7 @@
 var vows = require('vows');
 var assert = require('assert');
 var util = require('util');
-var AnonymousStrategy = require('../lib/passport-anonymous/strategy');
+var AnonymousStrategy = require('../lib/strategy');
 
 
 vows.describe('AnonymousStrategy').addBatch({
